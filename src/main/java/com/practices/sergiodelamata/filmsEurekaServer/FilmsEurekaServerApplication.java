f18c3927c858980ebaf470f315156cd739fb6b19
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-02 17:51:12 +0100

    :sparkles: Established the Eureka server configuration and properties.

--- src/main/java/com/practices/sergiodelamata/filmsEurekaServer/FilmsEurekaServerApplication.java
+++ src/main/java/com/practices/sergiodelamata/filmsEurekaServer/FilmsEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.practices.sergiodelamata.filmsEurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class FilmsEurekaServerApplication {
 
