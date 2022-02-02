97e67e6c34b9cf16469ab890f0d35d154de83bb6
SergiodelaMata <sergio.matam@edu.uah.es>
2022-02-02 17:29:32 +0100

    :sparkles: Established the base from the Spring Boot project.

---
+++ src/main/java/com/practices/sergiodelamata/filmsEurekaServer/FilmsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.practices.sergiodelamata.filmsEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmsEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practices/sergiodelamata/filmsEurekaServer/FilmsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practices.sergiodelamata.filmsEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
